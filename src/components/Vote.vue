ca793a08e77d3ebf3a8dc5e8f6c8555ccec8c26e
flotr18 <55090943+flotr18@users.noreply.github.com>
2022-01-21 01:29:26 +0100

    Updated Procfile

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -75,7 +75,7 @@ export default {
   },
   created() {
     if (localStorage.getItem('token') === null) {
-      this.$router.push('/login');
+      this.$router.push('/');
     }
   },
   mounted() {
