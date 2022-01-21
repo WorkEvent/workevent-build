2beb6e834761f451756646c797540604519614b5
flotr18 <55090943+flotr18@users.noreply.github.com>
2022-01-21 10:58:52 +0100

    Update info.vue

--- src/components/info.vue
+++ src/components/info.vue
@@ -35,17 +35,16 @@
     <div class="row">
       <div class="col-12 third-section">
         <div class="col-4 block-text">
-          <h1>Notre éthique</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h2>Notre éthique</h2>
+          <p></p>
         </div>
         <div class="col-4 block-text">
-          <h1>Nos Objectifs</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h2>Nos Objectifs</h2>
+          <p></p>
         </div>
         <div class="col-4 block-text">
-          <h1>Nos Valeurs</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
+          <h2>Nos Valeurs</h2>
+          <p></p>
       </div>
     </div>
     <div class="row">
